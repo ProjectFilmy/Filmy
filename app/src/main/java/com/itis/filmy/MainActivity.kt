e7cf0b00b1185d57eb1d77a18cb2d1cbbf2ce05f
Ilya <ignatium1356@gmail.com>
2024-07-07 01:13:09 +0300

    Fixed some bugs

--- app/src/main/java/com/itis/filmy/MainActivity.kt
+++ app/src/main/java/com/itis/filmy/MainActivity.kt
@@ -3,6 +3,8 @@ package com.itis.filmy
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -18,6 +20,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         binding = ActivityMainBinding.inflate(layoutInflater).also {
             setContentView(it.root)
